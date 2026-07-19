fe7c4a4ff53f587c4e81673518dc84aea2374cbd
Tim Tabachuk <timtabachuk7@gmail.com>
2026-07-18 20:38:41 -0400

    change BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes, HashRouter } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { NotFound } from './pages/NotFound'
 
@@ -10,13 +10,13 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
           <Route />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
