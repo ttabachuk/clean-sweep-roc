85d6410272074b2a69bc7e5f61b5e920b8910557
Tim Tabachuk <timtabachuk7@gmail.com>
2026-07-18 21:28:31 -0400

    patch: fix nav links appearing as not found

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import { Route, Routes, HashRouter } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { NotFound } from './pages/NotFound'
 
 function App() {
-
-  return (
-    <>
-      <HashRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-          <Route />
-        </Routes>
-      </HashRouter>
-    </>
-  )
+  return <Home />
 }
 
 export default App
